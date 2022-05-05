ceb410a8b41c890d63d93dabf87f892479bf1777
AidanDan4th <103073346+AidanDan4th@users.noreply.github.com>
2022-05-05 13:54:18 -0600

    Update ClueMenuCode.cpp

--- ClueMenuCode.cpp
+++ ClueMenuCode.cpp
@@ -1,5 +1,5 @@
 // ClueMenuCode.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Made by CDT Aidan Danforth
 
 #include <iostream>
 using namespace std;
@@ -7,18 +7,18 @@ using namespace std;
 int main()
 {
 	int numplayers;
-	char playerchoice;
-	int playermenu = 0;
+	char playerchoice; // waiting for the card distrubution function and note function
+	int playermenu = 0; // temporary numbers in order to determine funcitonality.
 
 
 
 	cout << "Welcome to the Game of Clue!" << endl;
 	retry:
-	cout << "How many players do you wish to play with? (Min: 2, Max: 6)" << endl;
+	cout << "How many players do you wish to play with? (Min: 2, Max: 6)" << endl; // makes this more user accessable
 	cin >> numplayers;
-	if ((numplayers < 2) || (numplayers > 6))
+	if ((numplayers < 2) || (numplayers > 6)) // ensures that we only get the same number of players as there are character pieces
 	{
-		cout << "Invalid amount of players, please retry" << endl; goto retry;
+		cout << "Invalid amount of players, please retry" << endl; goto retry; // goto allows to display the correct amount of players
 	}
 	else
 		numplayers = numplayers;
@@ -77,14 +77,14 @@ int main()
 	} while (playermenu = 10);
 
 quit:
-	cout << "Thank you for playing the game!" << endl << "Do you wish to play again? (Y/N)" << endl;
+	cout << "Thank you for playing the game!" << endl << "Do you wish to play again? (Y/N)" << endl;  // goes back to the same goto funciton
 	cin >> playerchoice;
 	if (playerchoice == 'Y')
 	{
 		goto retry;
 	}
 	else
-		cout << "Bye Loser! Hope you punch your mointor" << endl;
+		cout << "Bye Loser! Hope you punch your mointor" << endl; // :) Thanks Max for the great send-off message
 	
 	
 }
