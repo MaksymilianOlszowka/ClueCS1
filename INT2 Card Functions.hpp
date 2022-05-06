c64fcd69adcfb91ae4f8caffe4aa32f716b0ce96
MaksymilianOlszowka <102278626+MaksymilianOlszowka@users.noreply.github.com>
2022-05-05 22:49:50 -0600

    Create INT2 Card Functions.hpp

---
+++ INT2 Card Functions.hpp
@@ -0,0 +1,23 @@
+//
+//  Card Functions.hpp
+//  Integration 2
+//
+//  Created by Maksymilian Olszowka on 5/5/22.
+//
+
+#ifndef Card_Functions_hpp
+#define Card_Functions_hpp
+
+#include <string>
+#include <stdio.h>
+using namespace std;
+
+string setCharacters(void);
+
+string setWeapons(void);
+
+string setLocations(void);
+
+void getcaseFile(void);
+
+#endif /* Card_Functions_hpp */
